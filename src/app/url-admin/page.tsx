0428298551de9d3294c49f79d9cea262631ef330
Elvinos33 <elvinos33@duck.com>
2024-01-30 15:01:33 +0100

    Finished Header

---
+++ src/app/url-admin/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+
+export default function UrlAdmin() {
+  return (
+    <main className="absolute inset-0 bg-white">
+      <Header />
+    </main>
+  );
+}
