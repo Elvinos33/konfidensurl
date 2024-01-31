7f288451c18c0cd1490b93e3fcad344e4f8bf8d7
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-31 08:47:07 +0100

    moved liv folder to src

--- src/app/testing/front.tsx
+++ src/app/testing/front.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import React from 'react';
-import { Link, newLink, deleteLink } from '../lib/links';
+import { Link, newLink, deleteLink } from '@/lib/links';
 
-export default function Front({links}: {links: Link[]}) {
+export default function Front({ links }: { links: Link[] }) {
   const [q, setQ] = React.useState('');
 
   const linkData = {
@@ -18,20 +18,30 @@ export default function Front({links}: {links: Link[]}) {
         type='text'
         value={q}
         onChange={(e) => setQ(e.target.value)}
-        className="text-red-900"
-   />
+        className='text-red-900'
+      />
       <button onClick={() => newLink(linkData)} className='bg-red-400 p-2'>
         Legg til en ny url
       </button>
 
       <h1 className='text-3xl'>Alle linker laget:</h1>
       <div className='flex flex-col gap-2'>
-        {links.map((link) => 
-          <div key={link.path} className="flex items-center gap-2 bg-red-400 rounded-lg p-1">
-            Path: {link.path} - URL: {link.url} - Expires: {new Date(link.expires).toLocaleDateString("no-NO")} - Clicks: {link.clicks}
-            <button onClick={() => deleteLink(link.path)} className="border-black border-2 rounded-lg p-1">DEL</button>
+        {links.map((link) => (
+          <div
+            key={link.path}
+            className='flex items-center gap-2 bg-red-400 rounded-lg p-1'
+          >
+            Path: {link.path} - URL: {link.url} - Expires:{' '}
+            {new Date(link.expires).toLocaleDateString('no-NO')} - Clicks:{' '}
+            {link.clicks}
+            <button
+              onClick={() => deleteLink(link.path)}
+              className='border-black border-2 rounded-lg p-1'
+            >
+              DEL
+            </button>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,5 +1,5 @@
 import Front from './front';
-import { getAllLinks } from '../lib/links';
+import { getAllLinks } from '@/lib/links';
 
 export default async function Page() {
   const links = await getAllLinks();
