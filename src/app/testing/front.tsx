112a81326fbe006675468c470d125bd9137feeaa
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-31 18:33:54 +0100

    tracking av klikk funksjon ferdig, flytter redirecting til client

--- src/app/testing/front.tsx
+++ src/app/testing/front.tsx
@@ -6,6 +6,7 @@ import { Link, newLink, deleteLink } from '@/lib/links';
 export default function Front({ links }: { links: Link[] }) {
   const [q, setQ] = React.useState('');
 
+// objektet som blir sendt når man lager ny link
   const linkData = {
     url: 'https://www.eliasuran.dev',
     path: q,
@@ -14,6 +15,7 @@ export default function Front({ links }: { links: Link[] }) {
   return (
     <div className='flex flex-col gap-4'>
       <h1>elias tester</h1>
+      {/* input field for å gi navn til nye paths */}
       <input
         type='text'
         value={q}
@@ -27,19 +29,21 @@ export default function Front({ links }: { links: Link[] }) {
 
       <h1 className='text-3xl'>Alle linker laget:</h1>
       <div className='flex flex-col gap-2'>
+      {/* mapper over alle linker */}
         {links.map((link) => (
           <div
             key={link.path}
             className='flex items-center gap-2 bg-red-400 rounded-lg p-1'
           >
-            Path: {link.path} - URL: {link.url} - Expires:{' '}
+            <a className="text-blue-700 underline" href={`/${link.path}`}>Path: {link.path}</a> - URL: {link.url} - Expires:{' '}
             {new Date(link.expires).toLocaleDateString('no-NO')} - Clicks:{' '}
             {link.clicks}
+            {/* slett link */}
             <button
               onClick={() => deleteLink(link.path)}
               className='border-black border-2 rounded-lg p-1'
             >
-              DEL
+              DELETE
             </button>
           </div>
         ))}
--- src/lib/links.ts
+++ src/lib/links.ts
@@ -98,3 +98,24 @@ export async function getLink(path: string) {
   });
   return link;
 }
+
+// finner først antall clicks til linken med path-en fra param
+// etter det legger den til et klikk til linken (clicks field er 0 by default, så trenger ikke sjekke for null eller no sånt)
+export async function incrementClicks(path: string) {
+  const data = await prisma.links.findUnique({
+    where: {
+      path: path,
+    },
+    select: {
+      clicks: true,
+    },
+  });
+  await prisma.links.update({
+    where: {
+      path: path,
+    },
+    data: {
+      clicks: data.clicks + 1,
+    },
+  });
+}
