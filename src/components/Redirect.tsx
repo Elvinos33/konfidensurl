b1987a4582c7f8633bcdd90f4c81942ef2052f1d
Elias Rissanen Uran <elura001@osloskolen.no>
2024-03-20 20:54:33 +0100

    further ga4 implementation

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -1,32 +1,36 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
-import ReactGA from "react-ga4";
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import ReactGA from 'react-ga4';
 
 export default function Redirect({ url }: { url: string | undefined }) {
   const pathname = usePathname();
 
   useEffect(() => {
-    if (url) window.location.href = url;
-    ReactGA.initialize("G-L6ZR8P56VP");
-    ReactGA.send({ hitType: "pageview", page: url });
+    if (!url) return;
+    ReactGA.send({
+      hitType: 'pageview',
+      page: pathname,
+      title: `The path: ${pathname} was visited`,
+    });
+    window.location.href = url;
   }, []);
   return (
-    <div className="absolute inset-0 flex justify-center items-center">
+    <div className='absolute inset-0 flex items-center justify-center'>
       {url ? (
-        <div className="flex flex-col items-center">
-          <h1 className="text-4xl font-bold text-konfidens-green">
+        <div className='flex flex-col items-center'>
+          <h1 className='text-konfidens-green text-4xl font-bold'>
             Redirecting to:
           </h1>
-          <h2 className="text-2xl font-semibold text-konfidens-darkGreen">
+          <h2 className='text-konfidens-darkGreen text-2xl font-semibold'>
             {url}
           </h2>
         </div>
       ) : (
-        <div className="flex flex-col items-center">
-          <h1 className="text-4xl font-bold text-konfidens-green">404</h1>
-          <h2 className="text-2xl font-semibold text-konfidens-darkGreen">
+        <div className='flex flex-col items-center'>
+          <h1 className='text-konfidens-green text-4xl font-bold'>404</h1>
+          <h2 className='text-konfidens-darkGreen text-2xl font-semibold'>
             No URL found at {pathname}
           </h2>
         </div>
